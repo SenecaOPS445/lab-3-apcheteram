f70a7db12658e2936756ec73c4c79a8009c093c3
Andy Persaud-Cheteram <apersaud-cheteram@myseneca.ca>
2025-01-30 22:18:24 -0500

    Added Lab3d

---
+++ lab3d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author ID: apersaud-cheteram
+
+import subprocess
+
+def free_space():
+    p = subprocess.Popen(["df -h | grep '/$' | awk ' {print $4}'"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output = p.communicate()
+    return output[0].decode('utf-8').strip()
+
+print(free_space())
